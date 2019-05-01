816dee7a93d7bbead364bffcdf2a71fa9b47b74e
Daniel Redondo <danielredondosanchez@hotmail.com>
2019-05-01 20:25:09 +0200

    Maximize number of articles tweeted

--- botwikipedia_github.R
+++ botwikipedia_github.R
@@ -1,30 +1,49 @@
 # Load packages
-library(pageviews)
 library(dplyr)
+library(pageviews)
+
+# Number of articles
+n <- 15
 
 # We extract data from yesterday, and exclude special pages
 top <- top_articles(project = "es.wikipedia", start = Sys.Date() - 1) %>%
   filter(!grepl(":", article)) %>%
   head(n)
-
-# Number of articles
-n <- 7
+top$article <- gsub("_", " ", top$article)
 
 # Build the tweet
-top$article <- gsub("_", " ", top$article)
-tuit <- paste0("Top ", n, " Wikipedia de ayer [visitas]: ",
-               top$article[1], " [", top$views[1], "]")
+tuit <- paste0("Top ", n, " Wikipedia de ayer [visitas]: ", top$article[1], " [", top$views[1], "]")
 for(i in 2:n) tuit <- paste0(tuit, ", ", top$article[i], " [", top$views[i], "]")
 tuit <- paste0(tuit, ".")
 
-# Connect with Twitter (see details at https://danielredondo.com/posts/20190224_botwikipedia/)
+tuit
+nchar(tuit)
+
+while(nchar(tuit) > 280) {
+  # If it doesn´t fit in 280 characters, we lower n until it fits
+  n <- n - 1
+  
+  # We extract data from yesterday, and exclude special pages
+  top <- top_articles(project = "es.wikipedia", start = Sys.Date() - 1) %>%
+    filter(!grepl(":", article)) %>%
+    head(n)
+  top$article <- gsub("_", " ", top$article)
+  
+  # Build the tweet
+  tuit <- paste0("Top ", n, " Wikipedia de ayer [visitas]: ", top$article[1], " [", top$views[1], "]")
+  for(i in 2:n) tuit <- paste0(tuit, ", ", top$article[i], " [", top$views[i], "]")
+  tuit <- paste0(tuit, ".")
+  print(n)
+}
+
+# Conect with Twitter (see details at https://danielredondo.com/posts/20190224_botwikipedia/)
 library("rtweet")
 token <- create_token(
   app = "nombre de la aplicación",
-  consumer_key = "XXX",
-  consumer_secret = "XXX",
-  access_token = "XXX",
-  access_secret = "XXX")
+  consumer_key = "XXXX",
+  consumer_secret = "XXXXX",
+  access_token = "XXXXX",
+  access_secret = "XXXXX")
 
 # Post the tweet
 post_tweet(tuit)
